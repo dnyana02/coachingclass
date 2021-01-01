dd7d289b81165cb9af87390c26a9e01e00913642
Rushikesh Susar <64800815+Rushikeshs2@users.noreply.github.com>
2021-01-01 12:58:25 +0530

    Update settings.py

--- coaching/settings.py
+++ coaching/settings.py
@@ -13,8 +13,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 from django.contrib import messages
-#import dj_database_url
-#import django_heroku
+import dj_database_url
+import django_heroku
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
